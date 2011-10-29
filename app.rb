cb64e6e6f3bbbff5e38ae34e649ce097f41ed153
Amiel Martin <amiel.martin@gmail.com>
2011-10-28 17:59:27 -0700

    Need json lib, plain text for now

--- app.rb
+++ app.rb
@@ -10,19 +10,16 @@ require APP_ROOT.join('markov_chainer')
 mc = MarkovChainer.new 2
 
 get '/' do
-  content_type :json
-
-  { :response => mc.generate_scentence }.to_json
+  # content_type :json
+  mc.generate_sentence
 end
 
 
-post '/' do
-  content_type :json
-
+post '/add' do
+  # content_type :json
   if params[:words]
     mc.add_text params[:words]
   end
-
-  { :response => 'thanks' }.to_json
+  'thanks'
 end
 
