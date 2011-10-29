b1f06a38f0e3f449d28b53079ff83d2e8800dfab
Amiel Martin <amiel.martin@gmail.com>
2011-10-28 17:55:31 -0700

    Another approach

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source :rubygems
 
 gem 'sinatra'
-gem 'markov'
+# gem 'markov'
--- app.rb
+++ app.rb
@@ -4,9 +4,25 @@ require 'sinatra'
 APP_ROOT = Pathname.new(File.expand_path("..", __FILE__))
 # require APP_ROOT.join('app/models')
 
+
+require APP_ROOT.join('markov_chainer')
+
+mc = MarkovChainer.new 2
+
 get '/' do
   content_type :json
 
+  { :response => mc.generate_scentence }.to_json
 end
 
+
+post '/' do
+  content_type :json
+
+  if params[:words]
+    mc.add_text params[:words]
+  end
+
+  { :response => 'thanks' }.to_json
+end
 
